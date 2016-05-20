ba280093d2c8daa377123f90ada097eba0e8b320
Mohd Osamah <mohdosamah02@gmail.com>
2016-05-20 14:48:14 +0530

    Add files via upload

---
+++ Syncmefy/app/src/main/java/com/example/mohdosamah/syncmefy/Client.java
@@ -0,0 +1,22 @@
+package com.example.mohdosamah.syncmefy;
+
+/**
+ * Created by Shikhar on 18/05/16.
+ */
+public class Client {
+    int channelID;
+    public Client(int initID)
+    {
+        channelID=initID;
+    }
+    public void receiveFile()
+    {
+
+    }
+    public void connectToChannel(int c_id)
+    {
+        //get ID of channel from user
+        //
+    }
+
+}
--- Syncmefy/app/src/main/java/com/example/mohdosamah/syncmefy/MainActivity.java
+++ Syncmefy/app/src/main/java/com/example/mohdosamah/syncmefy/MainActivity.java
@@ -2,12 +2,48 @@ package com.example.mohdosamah.syncmefy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    Server theCreator;
+    Channel theConnector;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button ServerB=(Button)findViewById(R.id.button2);
+        Button serverSearch=(Button)findViewById(R.id.button);
+        ServerB.setOnClickListener(this);
+        serverSearch.setOnClickListener(this);
+
+
+
+    }
+    @Override
+    void onClick(View v)
+    {
+        if(v.getId()==R.id.button2 ){
+            if(theCreator!=NULL)return;
+            /*code for pressing server button*/
+            //TO-DO 1.set up a network
+            
+            //2.generate its ID
+            long temp_id=(long)(Math.random()*100);
+            //3.instantiate server object(theCreator) with that ID
+            theCreator=new Server(temp_id);
+            //if already istantiated ignore call
+        }
+        else{
+            /*code to search for channels*/
+            //make a client
+            //call its connect to channel
+
+        }
     }
 }
